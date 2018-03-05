7f28b3bdef9de5332541ab0adf102705a306d511
Erin Walker <erinb0317@hotmail.com>
2018-03-05 23:58:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books
-  INNER JOIN series ON series.id = books.series_id
+  "SELECT books.title, books.year 
+  FROM books
+  INNER JOIN series 
+  ON series.id = books.series_id
   WHERE series.id = (SELECT MIN(series.id) FROM series)
   ORDER BY books.year ASC;"
 end
@@ -21,7 +23,18 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title 
+  FROM series
+  JOIN books
+  ON books.series_id = series.id
+  JOIN character_books
+  ON character_books.book_id = books.id
+  JOIN characters
+  ON character_books.character_id = characters.id
+  WHERE characters.species = 'human' 
+  ORDER BY COUNT (*) DESC
+  LIMIT 1;
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
