f2487d7a4643bc9718f6f7dda1af313a7f2df442
Erin Walker <erinb0317@hotmail.com>
2018-03-06 00:06:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON character_books.character_id = characters.id
   WHERE characters.species = 'human'
-  GOUP BY series.title
+  GROUP BY series.title
   ORDER BY COUNT (*) DESC
   LIMIT 1;
   "
